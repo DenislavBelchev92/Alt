2b3bd987a554d50b6add1cf2d15e4e5fc9ffbc3d
denislav.belchev <denislav.belchev92@gmail.com>
2025-06-22 20:31:20 +0300

    Prepare for deploy on render 3

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+echo "->Install requirements"
 pip install -r requirements.txt
+echo "->Collect static files"
 python manage.py collectstatic --noinput
+echo "->Make migrations"
 python manage.py migrate
