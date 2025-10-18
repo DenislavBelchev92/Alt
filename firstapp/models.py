7af96af7bf2bfd1430bcc2dc485161ca4ca49cfa
denislav.belchev <denislav.belchev92@gmail.com>
2025-10-18 13:41:05 +0300

    Refactore skills tab

--- firstapp/models.py
+++ firstapp/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+def user_profile_picture_path(instance, filename):
+    """Generate upload path for user profile pictures"""
+    return f'profile_pics/{instance.user.username}_{filename}'
+
 class AltUser(AbstractUser):
 
     def get_skills(self):
@@ -23,7 +27,7 @@ class Profile(models.Model):
     age = models.CharField(max_length=100, blank=True)
     country = models.CharField(max_length=100, blank=True)
     city = models.CharField(max_length=100, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
